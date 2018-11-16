1b3704538665da6ede358900c3ea27a883bab00e
vietcuongk99 <daoquocthai8@gmail.com>
2018-11-16 23:31:41 +0700

    chưa chỉnh kỹ

--- src/uet/oop/bomberman/entities/character/Bomber.java
+++ src/uet/oop/bomberman/entities/character/Bomber.java
@@ -142,8 +142,8 @@ public class Bomber extends Character {
     @Override
     public boolean canMove(double x, double y) {
         // TODO: kiểm tra có đối tượng tại vị trí chuẩn bị di chuyển đến và có thể di chuyển tới đó hay không
-        System.out.println(_y + "::" + _x);
-        System.out.println(y + "::" + x);
+//        System.out.println(_y + "::" + _x);
+//        System.out.println(y + "::" + x);
         for(int c = 0; c < 4 ; c++){
             double xt = ((_x + x) + c % 2 * 11) / Game.TILES_SIZE; //divide with tiles size to pass to tile coordinate
             double yt = ((_y + y) + c / 2 * 12 - 13) / Game.TILES_SIZE; //these values are the best from multiple tests
