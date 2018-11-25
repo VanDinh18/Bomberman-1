fbcbf3d2f3ae31cb773b09d80a9445c6fbb0feb4
duong17020661 <43227742+duong17020661@users.noreply.github.com>
2018-11-25 20:24:33 +0700

    update Audio

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -3,13 +3,13 @@ package uet.oop.bomberman;
 import uet.oop.bomberman.gui.Frame;
 
 public class BombermanGame {
-	public static Audio theme = new Audio("Stage Theme.wav");
+	public static AudioLoop theme = new AudioLoop("Stage Theme.wav");
 
 	public static Audio complete = new Audio("Stage Complete.wav");
 	public static Audio game_over = new Audio("Game Over.wav");
 
-//	public static Audio audio = new Audio("bomberman_orchestra.wav");
 	public static void main(String[] args) {
+		theme.start();
 		new Frame();
 	}
 }
